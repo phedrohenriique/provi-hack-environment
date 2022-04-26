96527946913b36059c3af8e8f1127eda353ddba0
ALISON <superalison>
2022-04-26 03:06:20 -0300

    create front route Login

---
+++ src/pages/login/index.js
@@ -0,0 +1,13 @@
+import { Center } from '@chakra-ui/react'
+import LoginCard from '../../components/Logincard'
+
+function LoginPage() {
+
+    return (
+        <Center h={"100vh"}>
+            <LoginCard />
+        </Center>
+    )
+}
+
+export default LoginPage
