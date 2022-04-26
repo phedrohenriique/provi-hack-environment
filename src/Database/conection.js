2bdb6511877652c716bad5229b3b3fc7dc319ca8
ALISON <superalison>
2022-04-26 17:22:13 -0300

    Create instance unique supabase client

--- src/Database/conection.js
+++ src/Database/conection.js
@@ -1,18 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
 
-/*
-  falta ANON_KEY e HOST do banco de dados para usar o supabase
-*/
+const ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1Z2d2YmFwYnpiZnpndWZmcHF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA5MzM4NDgsImV4cCI6MTk2NjUwOTg0OH0.Wfn1Hy_WGjce3S6rUNChXbLTFnost1o1r0Thi96hGtc"
 
-const knex = require('knex')({
-    client: 'pg',
-    connection: {
-      host : 'db.quggvbapbzbfzguffpqt.supabase.co',
-      port : 6543,
-      user : 'postgres',
-      password : 'equipe7provihack',
-      database : 'postgres'
-    }
-  });
+const URL_SUPABASE =  "https://quggvbapbzbfzguffpqt.supabase.co"
 
+const supabase = createClient(URL_SUPABASE, ANON_KEY);
 
-module.exports = knex
+module.exports = supabase;
