8dd0089bfc719386150a668355816075d263803f
ALISON <superalison>
2022-04-27 02:39:42 -0300

    Protect route to invalid mathod

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -6,36 +6,42 @@ import jwt from "jsonwebtoken";
 
 export default async function loginRoute(req, res) {
     const { email, senha } = JSON.parse( req.body );
-
+    
     if (!email || !senha) {
         return res.status(404).json({message: 'É obrigatório email e senha'});
     }
     
-    try {
-        const {data}  = await conn.from("users").select("*").eq("email", email);
+    switch(req.method){
+        case "POST": // rota POST api/login
+            try {
+                const {data}  = await conn.from("users").select("*").eq("email", email);
+                
+               const [usuario] = data;
+               
+                if (!usuario) {
+                    return res.status(400).json({message: "O usuario não foi encontrado"});
+                }
+        //==============================        senha no banco tem que ser encriptada antes de salvarem ==========================
+                const senhaCorreta = await bcrypt.compare(senha, usuario.password); 
         
-       const [usuario] = data;
-       
-        if (!usuario) {
-            return res.status(400).json({message: "O usuario não foi encontrado"});
-        }
-//==============================        senha no banco tem que ser encriptada antes de salvarem ==========================
-        const senhaCorreta = await bcrypt.compare(senha, usuario.password); 
-
-        if (!senhaCorreta) {
-            return res.status(400).json({message:"Email e senha não confere"});
-        }
-
-     
-        const token = jwt.sign({ id: usuario.id }, secret, { expiresIn: '8h' });
-
-        const { senha: _, ...dadosUsuario } = usuario;
-
-        return res.status(200).json({
-            usuario: dadosUsuario,
-            token
-        });
-    } catch (error) {
-        return res.status(403).json( {...error});
+                if (!senhaCorreta) {
+                    return res.status(400).json({message:"Email e senha não confere"});
+                }
+        
+             
+                const token = jwt.sign({ id: usuario.id }, secret, { expiresIn: '8h' });
+        
+                const { senha: _, ...dadosUsuario } = usuario;
+        
+                return res.status(200).json({
+                    usuario: dadosUsuario,
+                    token
+                });
+            } catch (error) {
+                return res.status(403).json( {...error});
+            }
+        default:// caso em que o acesso tenha um method diferente de POST
+            return res.status(404).json( {message: "method invalid"} );
     }
+    
 }
