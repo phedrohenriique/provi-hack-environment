400e5cf553ff95a23d050c4e2977c0e628de6c7e
ALISON <superalison>
2022-04-28 02:46:32 -0300

    create componente nav bar page

---
+++ src/components/Menu/MenuDesktop/index.js
@@ -0,0 +1,26 @@
+import { Box, HStack, IconButton, VStack } from "@chakra-ui/react";
+import Link from "next/link";
+import { useState } from "react";
+import { RiCloseLine, RiMenu2Fill } from "react-icons/ri";
+import style from "../../../styles/menu.module.css"
+import LinkLocal from "../../LinkLocal";
+
+function MenuDesktop() {
+    return (
+        <box className={style.menuDesktop} >
+            <HStack 
+                spacing="3rem"
+            >
+                <LinkLocal href="#" >Quem somos nós</LinkLocal>
+                <LinkLocal href="#">Empresas parceiras</LinkLocal>
+                <LinkLocal href="#">Instituiçoes cadastradas</LinkLocal>
+                
+                <LinkLocal href="#" bg="gray" p="2">Quero me cadastrar</LinkLocal>
+               
+            </HStack>
+        </box>
+
+    )
+}
+
+export default MenuDesktop;
