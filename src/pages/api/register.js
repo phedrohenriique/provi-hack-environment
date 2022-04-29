9e263c9459b2a43e45adca8e571da4432f4d9fa6
Pedro Henrique <phd19994@gmail.com>
2022-04-28 22:34:01 -0300

    route working

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -1,17 +1,22 @@
-import { connection } from '../../Database/conection'
+import connection  from '../../Database/conection'
 
 // to acess the endpoint you must go thorugh route localhost:3000/api/register
 // all logic will be acessed
 
 export default async function registerRoute(req, res) {
-    const {name, cpf, password , email} = req.body
+    const {name, cpf, password, email} = req.body
 
     try{
-        //const rows = await connection.from("users").select()
-        //console.log(rows)
-        res.status(200).json({message: 'route working'})
+        
+        // acess connection methods to manipulate database
+        // data is the number of rows retrieved
+
+        const data = await connection.from("users").select('*')
+        console.log(data)
+        res.status(200).json(data)
     }
     catch{
         console.log('couldnt get rows')
+        return res.status(400).json({message: 'there was an error'})
     }
 }
