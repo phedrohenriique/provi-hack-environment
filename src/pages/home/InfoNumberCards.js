4a95aae744367e14f076f413ccabf8ff63d8d6d1
Pedro Henrique <phd19994@gmail.com>
2022-04-29 00:45:49 -0300

    minor card with big numbers created

---
+++ src/pages/home/InfoNumberCards.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {
+    Box,
+    Text
+} from '@chakra-ui/react'
+
+export default function InfoNumberCards({title, description}) {
+    const text = {
+        color: '#003B70',
+        fontFamily: 'sans-serif',
+        fontWeight: '600'
+    }
+
+    return (
+        <Box
+            minWidth='30vw'
+            minHeigth='30vh'
+            background='#7ADFA5'
+            display='flex'
+            flexDirection='column'
+            borderRadius='md'
+            padding='5%'
+        >
+            <Text style={text} fontSize='8xl'>{title}</Text>
+            <Text style={text} fontSize='xl'>{description}</Text>
+        </Box>
+    )
+}
