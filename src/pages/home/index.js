1a3f1d2af47e27cd158f31ea7b3877618aa6605e
Pedro Henrique <phd19994@gmail.com>
2022-04-29 21:55:39 -0300

    small changes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,17 @@
+import {
+  Box
+} from '@chakra-ui/react';
 import React from 'react'
 import InfoNumberCards from '../../components/InfoCards/InfoNumberCards';
 
 export default function HomePage() {
   return (
     <>
-    <InfoNumberCards 
-    title='+100' 
-    description='Pessoas conectadas à uma instituição' />
+      <Box>
+        <InfoNumberCards
+          title='+100'
+          description='Pessoas conectadas à uma instituição' />
+      </Box>
     </>
   )
 }
